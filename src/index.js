2777e4efaaf9122c92b773fe0c648ef86569f2e5
priya2771 <157040256+priya2771@users.noreply.github.com>
2025-02-04 16:39:41 +0530

    media

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
-/**
- * Entry point of application, where App is rendered within the div with the id of "app"
- */
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
-const container = document.getElementById("app");
-const root = createRoot(container);
-root.render(<App />);
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Check if reportWebVitals.js exists before importing
+// try {
+//   const reportWebVitals = require("./reportWebVitals").default;
+//   reportWebVitals();
+// } catch (error) {
+//   console.warn("reportWebVitals.js not found, skipping performance monitoring.");
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
